8dec6244e42fb98e45c023bef452361bb5322208
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-08-21 03:35:42 +0530

    created chessboard

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Chessboard from "./components/Chessboard"
+
+const App = () => {
+  return (
+    <div className="grid place-content-center h-[100vh] bg-black">
+      <Chessboard />
+    </div>
+  )
+}
+
+export default App
