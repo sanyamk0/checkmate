b4e933b97c7b3ae5288bf6e6f1a43deec8b6366a
Sanyam Kumar <sanyamkumar002@gmail.com>
2023-08-29 12:28:40 +0530

    added pieces movement functionality

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -25,7 +25,33 @@ for (let i = 0; i < 8; i++) {
 for (let i = 0; i < 8; i++) {
     pieces.push({ image: "assets/images/pawn_w.png", x: i, y: 1 })
 }
+let activePiece: HTMLElement | null = null;
 
+function grabPiece(e: React.MouseEvent) {
+    const element = e.target as HTMLElement
+    if (element.classList.contains("chess-piece")) {
+        const x = e.clientX - 50;
+        const y = e.clientY - 50;
+        element.style.position = "absolute";
+        element.style.left = `${x}px`;
+        element.style.top = `${y}px`;
+        activePiece = element;
+    }
+}
+function movePiece(e: React.MouseEvent) {
+    if (activePiece) {
+        const x = e.clientX - 50;
+        const y = e.clientY - 50;
+        activePiece.style.position = "absolute";
+        activePiece.style.left = `${x}px`;
+        activePiece.style.top = `${y}px`;
+    }
+}
+function dropPiece(e: React.MouseEvent) {
+    if (activePiece) {
+        activePiece = null;
+    }
+}
 const Chessboard = () => {
     const board = [];
     for (let j = verticalAxis.length - 1; j >= 0; j--) {
@@ -37,11 +63,16 @@ const Chessboard = () => {
                     image = p.image;
                 }
             })
-            board.push(<Tile image={image} number={number} />)
+            board.push(<Tile key={`${i}${j}`} image={image} number={number} />)
         }
     }
     return (
-        <div className="lg:h-[640px] lg:w-[640px] sm:w-[480px] sm:h-[480px] grid grid-cols-8 grid-rows-[8]">
+        <div
+            onMouseMove={(e) => movePiece(e)}
+            onMouseDown={e => grabPiece(e)}
+            onMouseUp={(e) => dropPiece(e)}
+            className="lg:h-[640px] lg:w-[640px] sm:w-[480px] sm:h-[480px] grid grid-cols-8 grid-rows-[8]"
+        >
             {board}
         </div>
     )
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,12 +1,19 @@
+import "./Tile.css"
 interface Props { image?: string, number: number }
 
 const Tile = ({ number, image }: Props) => {
     if (number % 2 === 0) {
         return (
-            <div className="bg-[#779556] lg:h-[80px] lg:w-[80px] sm:h-[60px] sm:w-[60px] grid place-content-center"><img src={image} className="w-[70px]" /></div>)
+            <div className="tile black-tile lg:h-[80px] lg:w-[80px] sm:h-[60px] sm:w-[60px]">
+                {image && <div className="chess-piece"
+                    style={{ backgroundImage: `url(${image})` }}></div>}
+            </div >)
     } else {
         return (
-            <div className="bg-[#ebecd0] lg:h-[80px] lg:w-[80px] sm:h-[60px] sm:w-[60px] grid place-content-center"><img src={image} className="w-[70px]" /></div>)
+            <div className="tile white-tile lg:h-[80px] lg:w-[80px] sm:h-[60px] sm:w-[60px]">
+                {image && <div className="chess-piece"
+                    style={{ backgroundImage: `url(${image})` }}></div>}
+            </div>)
     }
 }
 
